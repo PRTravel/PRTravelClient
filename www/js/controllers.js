57d7aa5be50b9f25ea33f951dc1bfd6453511330
Harry Hernandez <harry.hernandez@upr.edu>
2016-10-02 20:24:39 -0400

    Added attractions list and sidemenu

---
+++ www/js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('PRTravel.controllers', [])
+
+.controller('AttractionsCtrl', function($scope) {
+  $scope.attractions = [{
+    id: 0,
+    name: 'Flamenco Beach',
+    description: 'One of the most beutiful beaches in the world!',
+    image: 'img/flamenco.jpg'
+  }, {
+    id: 1,
+    name: 'Las Cascadas Water Park',
+    description: 'Water Park',
+    image: 'img/cascadas.jpg'
+  }, {
+    id: 2,
+    name: 'Toro Verde',
+    description: 'ToroVerde is a new Ecological Adventure Park',
+    image: 'img/toroverde.jpg'
+  }, {
+    id: 3,
+    name: 'El Yunque National Forest',
+    description: 'El Yunque National Forest is the only tropical rain forest in the national forest system.',
+    image: 'img/yunque.jpg'
+  }, {
+    id: 4,
+    name: 'Caja de Muertos Island',
+    description: 'Caja de Muertos, one of the islands of Puerto Rico just 4.8 miles off the central southern coast of the island and 1.5 hour drive from the capital city of San Juan.',
+    image: 'img/cajaDeMuertos.jpg'
+  }];
+});
