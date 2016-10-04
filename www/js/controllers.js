d9e425018f14b28810e27d7c899be9d4bb61b7ab
Harry Hernandez <harry.hernandez@upr.edu>
2016-10-04 11:30:35 -0400

    Implemented controller to the search bar.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,31 +4,26 @@ angular.module('PRTravel.controllers', [])
   $scope.attractions = [{
     id: 0,
     name: 'Flamenco Beach',
-    //description: 'One of the most beutiful beaches in the world!',
     location: 'Culebra',
     image: 'img/flamenco.jpg'
   }, {
     id: 1,
     name: 'Las Cascadas Water Park',
-    //description: 'Water Park',
     location: 'Aguadilla',
     image: 'img/cascadas.jpg'
   }, {
     id: 2,
     name: 'Toro Verde',
-    //description: 'ToroVerde is a new Ecological Adventure Park',
     location: 'Orocovis',
     image: 'img/toroverde.jpg'
   }, {
     id: 3,
     name: 'El Yunque National Forest',
-    //description: 'El Yunque National Forest is the only tropical rain forest in the national forest system.',
     location: 'Rio Grande',
     image: 'img/yunque.jpg'
   }, {
     id: 4,
     name: 'Caja de Muertos Island',
-    //description: 'Caja de Muertos, one of the islands of Puerto Rico just 4.8 miles off the central southern coast of the island and 1.5 hour drive from the capital city of San Juan.',
     location: 'Ponce',
     image: 'img/cajaDeMuertos.jpg'
   }];
@@ -38,9 +33,17 @@ angular.module('PRTravel.controllers', [])
         $window.alert(added);
   }
 })
-.controller('TabsCtrl', function($scope){
+.controller('TabsCtrl', function($scope, $window){
+
+  //Hide and show search bar.
   $scope.showMe = true;
   $scope.toggle = function() {
       $scope.showMe = !$scope.showMe;
   }
+
+  //Logic of the search.
+  $scope.search = function(){
+    $window.alert('Searched for ' + document.getElementById('input_text').value);
+  }
+
 });
